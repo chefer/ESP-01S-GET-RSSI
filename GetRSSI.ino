dfdc3d7cd59361a9a866d2a68d110fd82fc972ce
Cristian Schaeffer Greve <cristianschaeffer@gmail.com>
2020-12-02 22:59:13 -0300

    Comentando últimas alterações sobre wifi_station_get_rssi()

--- GetRSSI.ino
+++ GetRSSI.ino
@@ -8,8 +8,10 @@ DynamicJsonDocument getRSSIData() {
   AP apLog;
 
   strcpy(apLog.ssid, (char*)WiFi.SSID().c_str());
+
+  apLog.rssi = wifi_station_get_rssi();
   
-  if (scanAPs.scanInProgress != SCANNING) {
+  /*if (scanAPs.scanInProgress != SCANNING) {
     //Serial.printf("!SCANNING: %d\n", scanAPs.scanInProgress);
     apLog.rssi = wifi_station_get_rssi();
     rssiLast = apLog.rssi;
@@ -17,6 +19,7 @@ DynamicJsonDocument getRSSIData() {
     //Serial.printf("SCANNING: %d\n", scanAPs.scanInProgress);
     apLog.rssi = rssiLast;
     }
+    */
   
   int qualityTemp = constrain(apLog.rssi, -100, -50);
   apLog.quality = map(qualityTemp, -100, -50, 0, 100);
